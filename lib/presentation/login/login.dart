e358837a1df546afbb45d9d218ccbfa73083073e
Babiy7 <olegbabiy77@gmail.com>
2019-08-20 17:50:41 +0300

    Created a MaterialButton.

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -13,32 +13,54 @@ class _LoginUiState extends State<LoginUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(child: buildUi(context)),
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: buildUi(context));
   }
 
   Widget buildUi(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        TextFormField(
-          decoration: const InputDecoration(
-              labelText: 'Email', border: OutlineInputBorder()),
+    final textViewEmail = TextFormField(
+      decoration: const InputDecoration(
+          labelText: 'Email', border: OutlineInputBorder()),
+    );
+
+    final textViewPassword = TextFormField(
+      decoration: const InputDecoration(
+          labelText: 'Password', border: OutlineInputBorder()),
+    );
+
+    final loginButton = Material(
+      shadowColor: Colors.lightBlueAccent.shade100,
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(
+        minWidth: 400.0,
+        height: 42.0,
+        child: Text(
+          "Sign in",
+          style: TextStyle(fontSize: 16),
         ),
-        const SizedBox(height: 16.0),
-        TextFormField(
-          decoration: const InputDecoration(
-              labelText: 'Password', border: OutlineInputBorder()),
+        color: Colors.redAccent,
+      ),
+    );
+
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              textViewEmail,
+              const SizedBox(height: 16.0),
+              textViewPassword,
+              const SizedBox(height: 32.0),
+              loginButton
+            ],
+          ),
         ),
-      ],
+      ),
     );
   }
 
