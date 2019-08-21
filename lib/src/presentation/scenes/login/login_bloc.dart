c8e7bb8eb29085e0bbdc11e0d91c8461f953c11b
Babiy7 <olegbabiy77@gmail.com>
2019-08-21 14:41:34 +0300

    [XHAB-44] Created the LoginValidationsState.

--- lib/src/presentation/scenes/login/login_bloc.dart
+++ lib/src/presentation/scenes/login/login_bloc.dart
@@ -1,12 +1,18 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:xhabits/src/domain/validation/validation.dart';
 import 'login_state.dart';
 
 class LoginBloc {
   BehaviorSubject<LoginState> _loginStateSubject;
   Observable<LoginState> get loginStateObservable => _loginStateSubject.stream;
 
+  final _defaultTextInputState = ValidationResult(true, null);
+
   LoginBloc() {
-    this._loginStateSubject = BehaviorSubject<LoginState>.seeded(
-        LoginState(false, LoginResultState(false, null, false)));
+    this._loginStateSubject = BehaviorSubject<LoginState>.seeded(LoginState(
+        LoginValidationsState(_defaultTextInputState, _defaultTextInputState),
+        false,
+        LoginResultState(false, null, false)));
   }
+  void validate(String email, String password) {}
 }
--- lib/src/presentation/scenes/login/login_state.dart
+++ lib/src/presentation/scenes/login/login_state.dart
@@ -1,4 +1,11 @@
-class LoginValidationsState {}
+import 'package:xhabits/src/domain/validation/validation.dart';
+
+class LoginValidationsState {
+  final ValidationResult emailValidation;
+  final ValidationResult passwordValidation;
+
+  LoginValidationsState(this.emailValidation, this.passwordValidation);
+}
 
 class LoginResultState {
   final bool loggedIn;
@@ -9,9 +16,10 @@ class LoginResultState {
 }
 
 class LoginState {
-  // final LoginValidationsState validationsState;
+  final LoginValidationsState validationsState;
   final bool signInButtonEnabled;
   final LoginResultState loginResultState;
 
-  LoginState(this.signInButtonEnabled, this.loginResultState);
+  LoginState(
+      this.validationsState, this.signInButtonEnabled, this.loginResultState);
 }
