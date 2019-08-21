776b6280cb3a93167a5f18029a673e4a36fc4f97
vpaniush <volodymyr2507@gmail.com>
2019-08-21 12:37:21 +0300

    [XHAB-38] change main to solve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:xhabits/src/presentation/scenes/login/login.dart';
-
 import 'src/presentation/XHApp.dart';
 
 void main() => runApp(XHApp());
