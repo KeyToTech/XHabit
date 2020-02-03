37906c35b3f9a18895cf039b30d24285596b236d
phenybutum <phenybutum@gmail.com>
2020-02-03 14:01:44 +0200

    [XHAB-99] refactor colors

--- lib/src/presentation/scenes/splash/splash_screen.dart
+++ lib/src/presentation/scenes/splash/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:xhabits/src/data/api/firebase/firebase_auth_service.dart';
 import 'package:xhabits/src/domain/simple_check_user_is_signed_in_use_case.dart';
+import 'package:xhabits/src/presentation/XHColors.dart';
 import 'package:xhabits/src/presentation/scenes/home/home_screen.dart';
 import 'package:xhabits/src/presentation/scenes/auth/login/login_screen.dart';
 import 'splash_screen_bloc.dart';
@@ -71,7 +72,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 fontWeight: FontWeight.bold,
                 fontSize: 65.0,
                 decoration: TextDecoration.none,
-                color: Color.fromRGBO(255, 51, 103, 1.0),
+                color: XHColors.pink,
               ),
             ),
             Text('BUILD  A  BETTER  YOU',
