8ae92ed4e7cbb90382081f4013f4dd08f704b53b
vpaniush <volodymyr2507@gmail.com>
2020-02-14 12:28:16 +0200

    [XHAB-129] replaces magic numbers on save screen with constants defined in SizeConfig

--- lib/src/presentation/widgets/xh_stateful_button.dart
+++ lib/src/presentation/widgets/xh_stateful_button.dart
@@ -3,23 +3,24 @@ import 'package:flutter/material.dart';
 class XHStatefulButton {
   final String text;
   final double fontSize;
+  final EdgeInsetsGeometry padding;
   final bool buttonEnabled;
   final Color activeColor;
   final Color inactiveColor;
   final void Function() submit;
 
-  XHStatefulButton(this.text, this.fontSize, this.buttonEnabled,
+  XHStatefulButton(this.text, this.fontSize, this.padding, this.buttonEnabled,
       this.activeColor, this.inactiveColor, this.submit);
 
-
   Widget statefulButton() => FlatButton(
-      child: Text(
-        text,
-        style: TextStyle(
-          fontSize: fontSize,
-          color: buttonEnabled ? activeColor : inactiveColor,
+        padding: padding,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: buttonEnabled ? activeColor : inactiveColor,
+          ),
         ),
-      ),
-      onPressed: buttonEnabled ? submit : null,
-  );
+        onPressed: buttonEnabled ? submit : null,
+      );
 }
