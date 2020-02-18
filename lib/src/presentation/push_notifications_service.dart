fda1772a42bac2ad340e23f65826df4d0a819ea5
phenybutum <phenybutum@gmail.com>
2020-02-18 21:14:43 +0200

    [XHAB-113] updates app's logo

--- lib/src/presentation/push_notifications_service.dart
+++ lib/src/presentation/push_notifications_service.dart
@@ -15,7 +15,7 @@ class PushNotificationsService {
 
   void _init() {
     _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    var initializationSettingsAndroid = AndroidInitializationSettings('logo');
+    var initializationSettingsAndroid = AndroidInitializationSettings('ic_launcher');
     var initializationSettingsIOS = IOSInitializationSettings();
     var initializationSettings = InitializationSettings(
         initializationSettingsAndroid, initializationSettingsIOS);
