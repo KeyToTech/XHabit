ef4a883954bfcb8899797715b3b7def7c09a7a6b
phenybutum <phenybutum@gmail.com>
2020-02-19 23:04:15 +0200

    [XHAB-143] fixed problems that occur after connecting plugin

--- lib/main_mobile.dart
+++ lib/main_mobile.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:xhabits/src/data/api/firebase/firebase_database_service_mobile.dart';
 import 'config/app_config.dart';
 import 'src/presentation/XHApp.dart';
 
+
 void main() {
   Crashlytics.instance.log('YOUR LOG COMES HERE');
+
   WidgetsFlutterBinding.ensureInitialized();
   AppConfig.database = FirebaseDatabaseServiceMobile();
-  runApp(XHApp());
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  runZoned(() {
+    runApp(XHApp());
+  }, onError: Crashlytics.instance.recordError);
 }
