2411053de65770bb1503ec9862aebc948189d3d7
phenybutum <phenybutum@gmail.com>
2020-02-20 15:28:33 +0200

    [XHAB-143] implements crashlytics

--- lib/main_mobile.dart
+++ lib/main_mobile.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:xhabits/src/data/api/firebase/firebase_database_service_mobile.dart';
 import 'config/app_config.dart';
 import 'src/presentation/XHApp.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
+  Crashlytics.instance.enableInDevMode = false;
   WidgetsFlutterBinding.ensureInitialized();
   AppConfig.database = FirebaseDatabaseServiceMobile();
-  runApp(XHApp());
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  runZoned(() {
+    runApp(XHApp());
+  }, onError: Crashlytics.instance.recordError);
 }
+
