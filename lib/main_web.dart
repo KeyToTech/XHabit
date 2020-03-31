4e48e56f02cfbb8a5b1a0f0ec24ff50066715743
phenybutum <phenybutum@gmail.com>
2020-03-31 17:25:11 +0300

    [XHAB-138] test build

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:xhabits/src/data/api/firebase/firebase_database_service_web.dart';
-import 'config/app_config.dart';
-import 'config/firebase_config.dart';
-import 'src/presentation/XHApp.dart';
+//import 'package:flutter/material.dart';
+//import 'package:xhabits/src/data/api/firebase/firebase_database_service_web.dart';
+//import 'config/app_config.dart';
+//import 'config/firebase_config.dart';
+//import 'src/presentation/XHApp.dart';
 
 //void main() {
 //  firebaseWebConfig();
