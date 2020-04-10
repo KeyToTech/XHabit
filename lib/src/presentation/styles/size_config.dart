c253541a301545b78f4b0f026bf048d0bd59009e
phenybutum <phenybutum@gmail.com>
2020-04-10 14:33:38 +0300

    [XHAB-162] extracts sizes to SizeConfig

--- lib/src/presentation/styles/size_config.dart
+++ lib/src/presentation/styles/size_config.dart
@@ -4,6 +4,8 @@ class SizeConfig {
   static Size screenSize;
   static final double appBarButtonText = screenSize.height * 0.023;
   static final double appBarTitle = screenSize.height * 0.027;
+  static final double bottomBarIconSize = screenSize.height * 0.045;
+  static final double bottomBarTitlesSize = screenSize.height * 0.015;
   static final EdgeInsetsGeometry saveScreenPadding = EdgeInsets.symmetric(
     vertical: screenSize.height * 0.02,
     horizontal: screenSize.width * 0.035,
--- lib/src/presentation/widgets/xh_bottom_bar.dart
+++ lib/src/presentation/widgets/xh_bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xhabits/src/presentation/styles/XHColors.dart';
+import 'package:xhabits/src/presentation/styles/size_config.dart';
 import 'package:xhabits/src/presentation/widgets/xh_bottom_bar_bloc.dart';
 
 class XHBottomBar {
@@ -16,12 +17,12 @@ class XHBottomBar {
     stream: _xhBottomBarBloc.currentIndexObservable,
     builder: (context, snapshot) => BottomNavigationBar(
         backgroundColor: XHColors.darkGrey,
-        iconSize: 35,
+        iconSize: SizeConfig.bottomBarIconSize,
         onTap: _xhBottomBarBloc.currentIndexChanged,
         currentIndex: _xhBottomBarBloc.currentIndex,
         unselectedItemColor: XHColors.lightGrey,
-        unselectedFontSize: 12,
-        selectedFontSize: 12,
+        unselectedFontSize: SizeConfig.bottomBarTitlesSize,
+        selectedFontSize: SizeConfig.bottomBarTitlesSize,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.repeat),
