44273d3f31ebd3311107a273c64802797ff8d81e
phenybutum <phenybutum@gmail.com>
2020-05-12 20:19:23 +0300

    [XHAB-188] adds ability to upload userInfo from FB when user logins at the first time

--- lib/src/data/api/firebase/auth/firebase_auth_service.dart
+++ lib/src/data/api/firebase/auth/firebase_auth_service.dart
@@ -13,7 +13,8 @@ class FirebaseAuthService implements AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final _firestore = FirebaseStorage.instance;
   final _database = FirebaseDatabase.instance.reference();
-  final BehaviorSubject<bool> _imageUploadStatusSubject = BehaviorSubject<bool>();
+  final BehaviorSubject<bool> _imageUploadStatusSubject =
+      BehaviorSubject<bool>();
 
   @override
   Stream<XHAuthResult> signUp(String email, String password) {
@@ -53,8 +54,7 @@ class FirebaseAuthService implements AuthService {
     return Stream.fromFuture(getSignedInUser());
   }
 
-
-  void _uploadProfilePic(File image) async{
+  void _uploadProfilePic(File image) async {
     _imageUploadStatusSubject.sink.add(true);
     String userId = (await _auth.currentUser()).uid;
     var storageReference = _firestore
@@ -67,9 +67,17 @@ class FirebaseAuthService implements AuthService {
     _imageUploadStatusSubject.sink.add(false);
     var imageURL = await storageReference.getDownloadURL();
     var stringURL = imageURL.toString();
-    await _database.child(userId).child('image').set({
+    await _database
+        .child(userId)
+        .child('image')
+        .set({
       'url': stringURL
     });
+  }
+
+  void _uploadProfilePicByURL(String imageURL) async {
+    String userId = (await _auth.currentUser()).uid;
+    await _database.child(userId).child('image').set({'url': imageURL});
   }
 
   @override
@@ -79,33 +87,33 @@ class FirebaseAuthService implements AuthService {
   }
 
   @override
-  Stream<String> getProfilePic(){
+  Stream<String> getProfilePic() {
     getFuture() async {
       String userId = (await _auth.currentUser()).uid;
-      String result = ((await _database.child(userId)
-          .child('image')
-          .child('url')
-          .once())
-          .value as String);
+      String result = ((await _database
+              .child(userId)
+              .child('image')
+              .child('url')
+              .once())
+              .value as String);
       return result;
     }
     return Stream.fromFuture(getFuture());
   }
 
-
   @override
   void updateUsername(String username) async {
     FirebaseUser user = await _auth.currentUser();
-    await _database.child(user.uid).child('username').set(
-        username);
+    await _database.child(user.uid).child('username').set(username);
     print('username has been changed: $username');
   }
 
   @override
-  Stream<String> getUsername(){
+  Stream<String> getUsername() {
     getFuture() async {
       String userId = (await _auth.currentUser()).uid;
-      String result = ((await _database.child(userId)
+      String result = ((await _database
+          .child(userId)
           .child('username')
           .once())
           .value as String);
@@ -116,11 +124,12 @@ class FirebaseAuthService implements AuthService {
   }
 
   @override
-  Stream<String> getUserEmail(){
+  Stream<String> getUserEmail() {
     getFuture() async {
       String email = (await _auth.currentUser()).email;
       return email;
     }
+
     return Stream.fromFuture(getFuture());
   }
 
@@ -136,12 +145,40 @@ class FirebaseAuthService implements AuthService {
         print('FB signIn ${authResult.user.email}');
         user = User(authResult.user.uid, authResult.user.email);
         String un = authResult.user.displayName;
-        updateUsername(un);
+        String up = authResult.user.photoUrl;
+        await _firstUsernameUpload(user.userId, un);
+        await _firstImageUpload(user.userId, up);
       }
       return user;
     }
 
     return Stream.fromFuture(getFbSignedInUser());
+  }
+
+  Future<bool>_firstUsernameUpload(String uId, String un) async {
+    String username = (await _database
+        .child(uId)
+        .child('username')
+        .once())
+        .value as String;
+    if (username == null || username == 'empty') {
+    updateUsername(un);
+    }
+    return true;
+  }
+
+  Future<bool> _firstImageUpload(String uId, String imageURL) async {
+    String url = (await _database
+        .child(uId)
+        .child('image')
+        .child('url')
+        .once())
+        .value
+    as String;
+    if(url == null){
+    _uploadProfilePicByURL(imageURL + '?height=500');
+    }
+    return true;
   }
 
   @override
